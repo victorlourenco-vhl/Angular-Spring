17416af60078eac00213eb3803d386bd9d443a91
Victor Lourenço <victorhlo12@hotmail.com>
2023-11-15 20:16:35 -0300

    FEAT: Criando o método para cadastrar clientes

--- api/src/main/java/com/spring/api/controllers/ClienteController.java
+++ api/src/main/java/com/spring/api/controllers/ClienteController.java
@@ -1,14 +1,23 @@
 package com.spring.api.controllers;
 
+import com.spring.api.entities.Cliente;
+import com.spring.api.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
 
-    @GetMapping
-    public String teste() {
-        return "olá, mundo!";
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping(value = "cadastro")
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return clienteService.cadastrar(cliente);
     }
 
 }
--- api/src/main/java/com/spring/api/services/ClienteService.java
+++ api/src/main/java/com/spring/api/services/ClienteService.java
@@ -1,7 +1,17 @@
 package com.spring.api.services;
 
+import com.spring.api.entities.Cliente;
+import com.spring.api.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente cadastrar(Cliente obj) {
+        return clienteRepository.save(obj);
+    }
 }
