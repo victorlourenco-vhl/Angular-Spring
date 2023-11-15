14233ffd6a40b564873464db744d433ce5938ec8
Victor Lourenço <victorhlo12@hotmail.com>
2023-11-15 20:18:37 -0300

    FEAT: Adicionando a annotation para CrossOrigin

--- api/src/main/java/com/spring/api/controllers/ClienteController.java
+++ api/src/main/java/com/spring/api/controllers/ClienteController.java
@@ -3,13 +3,11 @@ package com.spring.api.controllers;
 import com.spring.api.entities.Cliente;
 import com.spring.api.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clientes")
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
